f842b548d326ffeb1fcb3644734cb09d14233a95
oussama abouzouhour <abzhrofficiel@gmail.com>
2023-04-21 10:24:43 +0000

    ab

---
+++ lib/WeatherDetails.dart
@@ -0,0 +1,110 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter/material.dart';
+
+class WeatherDetails extends StatefulWidget{
+  final String myCity;
+  const WeatherDetails({required this.myCity});
+
+  @override
+  State<WeatherDetails> createState() => WeatherDetails_State();
+
+}
+
+class WeatherDetails_State extends State<WeatherDetails> {
+
+  //final String city;
+  //dynamic weatherData;
+  String temp="";int deg=0;
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _getWeatherData();
+  }
+
+  //late String url="https://api.openweathermap.org/data/2.5/weather?q=${widget.myCity}&appid=37234c0dc03a125fd544cfc5a9f05b5f&units=metric%27;";
+
+
+
+  //static get myCity => null;
+
+ /* void _getWeatherData(){
+
+    http.get(Uri.parse(url))
+        .then((data){
+      weatherData=jsonDecode(data.body);
+
+      print(weatherData);
+
+      setState(() {
+        //print(weatherData);
+        temp=weatherData["weather"][0]["main"];
+         deg= weatherData["main"]["feels_like"];
+      });
+    })
+        .catchError((err){
+      print('error');
+    });
+
+
+  }*/
+
+  Future<void> _getWeatherData() async {
+    final response = await http.get(Uri.parse(
+        "https://api.openweathermap.org/data/2.5/weather?q=${widget.myCity}&appid=37234c0dc03a125fd544cfc5a9f05b5f&units=metric%27;"
+    ));
+    final jsonData = json.decode(response.body);
+
+    setState(() {
+      temp = jsonData['weather'][0]['description'];
+      deg = jsonData['main']['temp'].toInt();
+    });
+  }
+
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('${widget.myCity} Details'),
+
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            '${widget.myCity}',
+            style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 16),
+          Text(
+            'Weather: $temp',
+            style: TextStyle(fontSize: 24),
+          ),
+          SizedBox(height: 16),
+          Text(
+            'Temp: $deg°C',
+            style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
